1592a2f53c90bf2786c8b6afb910a3dd0bd48c43
lehuygiang28 <lehuygiang28@gmail.com>
2024-09-03 23:14:23 +0700

    feat(fe): :zap: add play button

--- apps/fe/src/components/swiper/movie/index.tsx
+++ apps/fe/src/components/swiper/movie/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import type { CSSProperties } from 'react';
-import { Typography, Image, Row, Col, Space, Grid, Tag } from 'antd';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Typography, Row, Col, Space, Grid, Tag } from 'antd';
 import { CalendarOutlined } from '@ant-design/icons';
 import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
 import { EffectFade, Pagination, Autoplay } from 'swiper/modules';
@@ -46,7 +48,7 @@ export const MovieSwiper: React.FC<MovieSwiperProps> = ({ movies }) => {
         width: '100%',
         height: md ? '85vh' : '55vh',
         zIndex: 0,
-        transition: 'background-image 0.5s ease', // Add transition for background image
+        transition: 'background-image 0.5s ease',
     };
 
     const contentStyle: CSSProperties = {
@@ -57,9 +59,9 @@ export const MovieSwiper: React.FC<MovieSwiperProps> = ({ movies }) => {
     };
 
     const posterStyle: CSSProperties = {
-        borderRadius: '1rem',
+        borderRadius: '0.5rem',
         boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
-        transition: 'transform 0.5s ease', // Add transition for smooth slide
+        transition: 'transform 0.5s ease',
         width: '100%',
         height: '100%',
         maxWidth: '25rem',
@@ -68,8 +70,7 @@ export const MovieSwiper: React.FC<MovieSwiperProps> = ({ movies }) => {
     };
 
     const textContentStyle: CSSProperties = {
-        padding: '1rem',
-        borderRadius: '8px',
+        padding: '0',
         color: '#fff',
         textAlign: 'left',
     };
@@ -163,93 +164,123 @@ export const MovieSwiper: React.FC<MovieSwiperProps> = ({ movies }) => {
             >
                 {movies?.map((movie) => (
                     <SwiperSlide key={movie.slug}>
-                        <div style={heroStyle}>
-                            <div style={contentStyle}>
-                                <Row
-                                    justify="center"
-                                    align="middle"
-                                    style={{ height: '100%', width: '100%' }}
-                                >
-                                    <Col xs={{ span: 12 }} md={{ span: 16 }}>
-                                        <div style={textContentStyle} className="textContent">
-                                            <Space direction="vertical">
-                                                <div>
-                                                    <Title
-                                                        level={md ? 1 : 4}
-                                                        style={{ marginBottom: '0' }}
-                                                    >
-                                                        {movie.name}
-                                                    </Title>
-                                                    <Text style={{ color: 'rgb(156, 163, 175)' }}>
-                                                        {movie.originName}
-                                                    </Text>
-                                                    <div style={{ marginTop: '0.5rem' }}>
-                                                        <Space size={'middle'}>
-                                                            <span>
-                                                                <CalendarOutlined
-                                                                    style={{
-                                                                        fontSize: '0.8rem',
-                                                                    }}
-                                                                />
+                        <Link href={`/phim/${movie.slug}`}>
+                            <div style={heroStyle}>
+                                <div style={contentStyle}>
+                                    <Row
+                                        justify="center"
+                                        align="middle"
+                                        style={{ height: '100%', width: '100%' }}
+                                    >
+                                        <Col xs={{ span: 14 }} md={{ span: 18 }}>
+                                            <div style={textContentStyle} className="textContent">
+                                                <Space
+                                                    direction="vertical"
+                                                    size={md ? 'middle' : 'small'}
+                                                >
+                                                    <div>
+                                                        <Title
+                                                            level={md ? 1 : 4}
+                                                            style={{ marginBottom: '0' }}
+                                                        >
+                                                            {movie.name}
+                                                        </Title>
+                                                        <Text type="secondary">
+                                                            {movie.originName}
+                                                        </Text>
+                                                        <div style={{ marginTop: '0.5rem' }}>
+                                                            <Space size={md ? 'middle' : 'small'}>
+                                                                <span>
+                                                                    <CalendarOutlined
+                                                                        style={{
+                                                                            fontSize: '0.8rem',
+                                                                        }}
+                                                                    />
+                                                                    <Text
+                                                                        style={{
+                                                                            fontSize: '0.8rem',
+                                                                        }}
+                                                                    >
+                                                                        {' '}
+                                                                        {movie.year}
+                                                                    </Text>
+                                                                </span>
                                                                 <Text
                                                                     style={{ fontSize: '0.8rem' }}
                                                                 >
-                                                                    {' '}
-                                                                    {movie.year}
+                                                                    |
                                                                 </Text>
-                                                            </span>
-                                                            <Text style={{ fontSize: '0.8rem' }}>
-                                                                |
-                                                            </Text>
-                                                            <Text style={{ fontSize: '0.8rem' }}>
-                                                                {movie.episodeCurrent}
-                                                            </Text>
-                                                        </Space>
+                                                                <Text
+                                                                    style={{ fontSize: '0.8rem' }}
+                                                                >
+                                                                    {movie.episodeCurrent}
+                                                                </Text>
+                                                            </Space>
+                                                        </div>
+                                                        <div style={{ marginTop: '0.5rem' }}>
+                                                            {movie?.categories?.map((category) => (
+                                                                <Tag
+                                                                    key={category?._id?.toString()}
+                                                                    style={{
+                                                                        fontSize: md
+                                                                            ? '0.7rem'
+                                                                            : '0.5rem',
+                                                                        background:
+                                                                            'rgba(0 0 0 / 0.4)',
+                                                                        border: 'none',
+                                                                    }}
+                                                                >
+                                                                    {category?.name}
+                                                                </Tag>
+                                                            ))}
+                                                        </div>
                                                     </div>
-                                                    <div style={{ marginTop: '0.5rem' }}>
-                                                        {movie?.categories?.map((category) => (
-                                                            <Tag
-                                                                key={category?._id?.toString()}
-                                                                style={{
-                                                                    fontSize: md
-                                                                        ? '0.7rem'
-                                                                        : '0.5rem',
-                                                                    background: 'rgba(0 0 0 / 0.4)',
-                                                                    border: 'none',
-                                                                }}
-                                                            >
-                                                                {category?.name}
-                                                            </Tag>
-                                                        ))}
-                                                    </div>
+                                                    {md && (
+                                                        <Paragraph
+                                                            ellipsis={{
+                                                                rows: md ? 5 : 3,
+                                                                expandable: false,
+                                                            }}
+                                                            style={{ maxWidth: '45vw' }}
+                                                        >
+                                                            {movie.content}
+                                                        </Paragraph>
+                                                    )}
+                                                </Space>
+                                            </div>
+                                            {md ? (
+                                                <div
+                                                    style={{
+                                                        background: '#bd1010',
+                                                        borderRadius: '9999px',
+                                                        maxWidth: '4rem',
+                                                        maxHeight: '4rem',
+                                                    }}
+                                                >
+                                                    <Image
+                                                        alt="play button"
+                                                        src="/assets/play.svg"
+                                                        width={64}
+                                                        height={64}
+                                                    />
                                                 </div>
-                                                {md && (
-                                                    <Paragraph
-                                                        ellipsis={{
-                                                            rows: 3,
-                                                            expandable: false,
-                                                            symbol: 'Xem th\u00eam',
-                                                        }}
-                                                        style={{ maxWidth: '45vw' }}
-                                                    >
-                                                        {movie.content}
-                                                    </Paragraph>
-                                                )}
-                                            </Space>
-                                        </div>
-                                    </Col>
-                                    <Col xs={{ span: 12 }} md={{ span: 8 }}>
-                                        <HigherHeightImage
-                                            url1={movie?.thumbUrl}
-                                            url2={movie?.posterUrl}
-                                            alt={movie?.name}
-                                            style={posterStyle}
-                                            className="posterImage"
-                                        />
-                                    </Col>
-                                </Row>
+                                            ) : (
+                                                <></>
+                                            )}
+                                        </Col>
+                                        <Col xs={{ span: 10 }} md={{ span: 6 }}>
+                                            <HigherHeightImage
+                                                url1={movie?.thumbUrl}
+                                                url2={movie?.posterUrl}
+                                                alt={movie?.name}
+                                                style={posterStyle}
+                                                className="posterImage"
+                                            />
+                                        </Col>
+                                    </Row>
+                                </div>
                             </div>
-                        </div>
+                        </Link>
                     </SwiperSlide>
                 ))}
             </Swiper>
