884e67739559c634b733e3043caf6d41608f9623
lehuygiang28 <lehuygiang28@gmail.com>
2024-09-08 00:55:50 +0700

    feat(api): :zap: optimize perfomance search movies

--- apps/api/src/app/movies/movie.service.ts
+++ apps/api/src/app/movies/movie.service.ts
@@ -15,31 +15,19 @@ export class MovieService {
     }
 
     async getMovies(dto: GetMoviesDto) {
-        const { keywords, cinemaRelease, isCopyright, type, years, categories, countries } = dto;
-
-        let filters: FilterQuery<Movie> = {
-            ...(cinemaRelease && { cinemaRelease }),
-            ...(isCopyright && { isCopyright }),
-            ...(type && { type }),
-            ...(years && { year: { $in: years?.split(',').map((year) => Number(year)) } }),
-        };
-
-        const keywordFilters = [];
-        if (keywords) {
-            const keywordRegex = createRegex(keywords, { outputCase: 'lowerAndUpper' });
-            keywordFilters.push({ name: keywordRegex });
-            keywordFilters.push({ originName: keywordRegex });
-            keywordFilters.push({ content: keywordRegex });
-            keywordFilters.push({ slug: keywordRegex });
-            keywordFilters.push({ 'categories.name': keywordRegex });
-            keywordFilters.push({ 'countries.name': keywordRegex });
-            keywordFilters.push({ 'actors.name': keywordRegex });
-            keywordFilters.push({ 'directors.name': keywordRegex });
-        }
-
-        if (keywordFilters.length > 0) {
-            filters = { ...filters, $or: keywordFilters };
-        }
+        const {
+            keywords,
+            cinemaRelease,
+            isCopyright,
+            type,
+            years,
+            categories,
+            countries,
+            limit = 10,
+            page = 1,
+            sortBy = 'year',
+            sortOrder = 'asc',
+        } = dto;
 
         const pipeline: PipelineStage[] = [
             {
@@ -94,31 +82,62 @@ export class MovieService {
                     ],
                 },
             },
-            {
-                $match: {
-                    ...filters,
-                    ...(keywordFilters.length > 0 && { $or: keywordFilters }),
-                    ...(categories?.length > 0 && {
-                        'categories.slug': { $in: categories?.split(',') },
-                    }),
-                    ...(countries?.length > 0 && {
-                        'countries.slug': { $in: countries?.split(',') },
-                    }),
-                },
-            },
-            { $sort: { [dto?.sortBy || 'year']: dto?.sortOrder === 'asc' ? 1 : -1 } },
+        ];
+
+        const match: FilterQuery<Movie> = {};
+
+        if (keywords) {
+            const keywordRegex = createRegex(keywords, { outputCase: 'lowerAndUpper' });
+            match.$or = [
+                { name: { $regex: keywordRegex } },
+                { originName: { $regex: keywordRegex } },
+                { content: { $regex: keywordRegex } },
+                { slug: { $regex: keywordRegex } },
+                { 'categories.name': { $regex: keywordRegex } },
+                { 'countries.name': { $regex: keywordRegex } },
+                { 'actors.name': { $regex: keywordRegex } },
+                { 'directors.name': { $regex: keywordRegex } },
+            ];
+        }
+
+        if (cinemaRelease !== undefined) {
+            match.cinemaRelease = cinemaRelease;
+        }
+
+        if (isCopyright !== undefined) {
+            match.isCopyright = isCopyright;
+        }
+
+        if (type) {
+            match.type = type;
+        }
+
+        if (years) {
+            match.year = { $in: years.split(',').map(Number) };
+        }
+
+        if (categories) {
+            match['categories.slug'] = { $in: categories.split(',') };
+        }
+
+        if (countries) {
+            match['countries.slug'] = { $in: countries.split(',') };
+        }
+
+        pipeline.push({ $match: match });
+
+        pipeline.push(
+            { $sort: { [sortBy]: sortOrder === 'asc' ? 1 : -1 } },
+            { $skip: (page - 1) * limit },
+            { $limit: limit },
             {
                 $facet: {
-                    movies: [
-                        { $project: { __v: 0, episode: 0 } },
-                        { $skip: (dto.page - 1) * dto.limit },
-                        { $limit: Number(dto.limit) },
-                    ],
+                    movies: [{ $project: { __v: 0, episode: 0 } }],
                     total: [{ $count: 'count' }],
                 },
             },
             { $project: { movies: 1, total: { $arrayElemAt: ['$total.count', 0] } } },
-        ];
+        );
 
         const result = (await this.movieRepo.aggregate<
             {
