5d74b2c63fe063ddc8a2a4050b379b8d26a28c54
lehuygiang28 <lehuygiang28@gmail.com>
2024-09-15 03:34:37 +0700

    refactor(fe): :zap: avoid waring `any` type for more flexible

--- apps/fe/src/hooks/useAxiosAuth.ts
+++ apps/fe/src/hooks/useAxiosAuth.ts
@@ -51,6 +51,7 @@ export function useAxiosAuth(payload?: UseAxiosAuthPayload) {
                 // Check for GraphQL errors
                 if (response?.data?.errors) {
                     const unauthorizedError = response.data.errors.find(
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
                         (error: any) =>
                             error?.message?.toLowerCase() === 'unauthorized' ||
                             error?.extensions?.code === 'UNAUTHENTICATED',
