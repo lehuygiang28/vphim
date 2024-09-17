d4f074e58a3b37560d929804b91eedccec0bd177
lehuygiang28 <lehuygiang28@gmail.com>
2024-09-18 00:59:41 +0700

    feat(mnt): :zap: add next-auth type declaration for user session

---
+++ apps/mnt/src/types/next-auth.d.ts
@@ -0,0 +1,13 @@
+import { DefaultJWT } from 'next-auth/jwt';
+import type { LoginResponseDto } from 'apps/api/src/app/auth/dtos';
+
+declare module 'next-auth' {
+    /**
+     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    declare interface Session {
+        user: LoginResponseDto;
+    }
+
+    interface JWT extends DefaultJWT, LoginResponseDto {}
+}
