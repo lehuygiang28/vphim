5f5ea3cb05ae69bc00de7e03eadf2545e9ce4b07
lehuygiang28 <lehuygiang28@gmail.com>
2024-10-14 03:51:05 +0700

    chore(api): :recycle: switch to using pm2 instead of supervisord
    [docker-latest]

---
+++ containers/be-redis-cloudflared/ecosystem.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    apps: [
+        {
+            name: 'cloudflared',
+            script: 'cloudflared',
+            args: 'tunnel --no-autoupdate run',
+            env: {
+                TUNNEL_TOKEN: process.env.CLOUDFLARED_TOKEN || 'giang',
+            },
+            output: '/dev/stdout',
+            error: '/dev/stderr',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+            autorestart: true,
+        },
+        {
+            name: 'redis',
+            script: '/usr/bin/redis-server',
+            args:
+                '--port ' +
+                (process.env.REDIS_PORT || '6379') +
+                ' --requirepass ' +
+                (process.env.REDIS_PASSWORD || 'giang'),
+            output: '/dev/stdout',
+            error: '/dev/stderr',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+            autorestart: true,
+        },
+        {
+            name: 'vphim-api',
+            script: '/usr/src/app/dist/apps/api/main.js',
+            instances: 'max',
+            exec_mode: 'cluster',
+            max_memory_restart: '4G',
+            output: '/dev/stdout',
+            error: '/dev/stderr',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+            autorestart: true,
+        },
+    ],
+};
