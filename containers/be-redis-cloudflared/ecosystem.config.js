4311f005ee6e8b0673c088d25dd89ec20aa2b568
lehuygiang28 <lehuygiang28@gmail.com>
2024-10-14 16:44:31 +0700

    chore(api): :recycle: update supervisor
    [docker-latest]

--- containers/be-redis-cloudflared/ecosystem.config.js
+++ containers/be-redis-cloudflared/ecosystem.config.js
@@ -1,26 +1,10 @@
 module.exports = {
     apps: [
         {
-            namespace: 'proxy',
-            name: 'cloudflared',
-            script: '/usr/local/bin/cloudflared',
-            args: 'tunnel --no-autoupdate --metrics localhost:9000 --protocol http2 run',
-            env: {
-                TUNNEL_TOKEN: process.env.CLOUDFLARED_TOKEN || 'giang',
-                DNS_SERVER: '1.1.1.1',
-            },
-            instances: 1,
-            exec_mode: 'fork',
-            output: '/dev/stdout',
-            error: '/dev/stderr',
-            log_date_format: 'YYYY-MM-DD HH:mm:ss',
-            autorestart: true,
-        },
-        {
             namespace: 'apps',
             name: 'vphim-api',
             script: '/usr/src/app/dist/apps/api/main.js',
-            instances: 1,
+            instances: 4,
             exec_mode: 'cluster',
             max_memory_restart: '2G',
             output: '/dev/stdout',
--- containers/be-redis-cloudflared/entrypoint.sh
+++ containers/be-redis-cloudflared/entrypoint.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
 
-# Load environment variables from .env file if it exists
-if [ -f .env ]; then
-  # Use `set -o allexport` and `set +o allexport` to ensure spaces are handled properly
-  set -o allexport
-  source .env
-  set +o allexport
-fi
-
-# Check if PM2_ID and PM2_SEC are set
-if [ -n "$PM2_ID" ] && [ -n "$PM2_SEC" ]; then
-  pm2 link "$PM2_ID" "$PM2_SEC"
-else
-  echo "PM2_ID and PM2_SEC must be set to link to PM2.io"
-fi
-
+# Start supervisord
 exec /usr/bin/supervisord -c /usr/src/app/supervisord.conf
