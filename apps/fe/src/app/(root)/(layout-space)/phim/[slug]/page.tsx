b5043050878b9c8abb76c67881f3c3e2be2a2178
lehuygiang28 <lehuygiang28@gmail.com>
2024-10-27 23:42:22 +0700

    refactor(fe): :zap: remove unused

--- apps/fe/src/app/(root)/(layout-space)/phim/[slug]/page.tsx
+++ apps/fe/src/app/(root)/(layout-space)/phim/[slug]/page.tsx
@@ -3,55 +3,13 @@ import { Metadata, ResolvingMetadata } from 'next';
 
 import { Movie } from '@/components/pages/movie';
 import { getOptimizedImageUrl } from '@/libs/utils/movie.util';
-import { getMovieBySlug, getMovies } from '@/services/movies';
-import { MOVIES_LIST_QUERY_FOR_ISR } from '@/queries/movies';
-
-// Add this export to enable ISR
-export const revalidate = 3600; // Revalidate every hour
+import { getMovieBySlug } from '@/services/movies';
 
 export const dynamic = 'force-dynamic';
 export const dynamicParams = true;
 
-// Set this to a number to limit the maximum number of pages fetched,
-// or set to null to fetch all pages
-const MAX_PAGES: number | null = null;
-
 export async function generateStaticParams() {
-    const uniqueSlugs = new Set<string>();
-    const pageSize = 500;
-    let currentPage = 1;
-    let hasMoreMovies = true;
-
-    while (hasMoreMovies && (MAX_PAGES === null || currentPage <= MAX_PAGES)) {
-        try {
-            const movies = await getMovies({
-                gqlQuery: MOVIES_LIST_QUERY_FOR_ISR,
-                operation: 'movies',
-                filters: [],
-                sorters: { field: 'view', order: 'desc' },
-                pagination: { current: currentPage, pageSize },
-            });
-
-            movies.forEach((movie) => uniqueSlugs.add(movie.slug));
-
-            // Check if we've reached the end of available data
-            hasMoreMovies = movies.length === pageSize;
-            currentPage++;
-        } catch (error) {
-            console.error(`Error fetching movies, page ${currentPage}:`, error);
-            hasMoreMovies = false;
-        }
-    }
-
-    if (MAX_PAGES !== null && currentPage > MAX_PAGES) {
-        console.log(`Reached maximum number of pages (${MAX_PAGES})`);
-    } else if (!hasMoreMovies) {
-        console.log('Fetched all available movies');
-    }
-
-    console.log(`Total unique movies fetched: ${uniqueSlugs.size}`);
-
-    return Array.from(uniqueSlugs).map((slug) => ({ slug }));
+    return [];
 }
 
 export type MoviePageProps = {
