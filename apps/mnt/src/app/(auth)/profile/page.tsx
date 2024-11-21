5fa22a712f223cc6b9555c57ac5da9fe3197583f
lehuygiang28 <lehuygiang28@gmail.com>
2024-11-21 11:46:25 +0700

    feat(mnt): :sparkles: add profile page with user update functionality and authentication check

---
+++ apps/mnt/src/app/(auth)/profile/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { Modal } from 'antd';
+import { useIsAuthenticated } from '@refinedev/core';
+
+import { LoadingSpinner } from '~fe/components/loading';
+import { UserUpdateComponent } from '~fe/components/pages/user/update';
+
+export default function ProfileParallelPage() {
+    const router = useRouter();
+    const { data: isAuthData, isLoading: isLoadingAuth } = useIsAuthenticated();
+
+    const handleClose = () => {
+        router.back();
+    };
+
+    if (!isAuthData?.authenticated) {
+        router.push('/');
+        return <></>;
+    }
+
+    return (
+        <Modal
+            open={true}
+            onCancel={handleClose}
+            cancelButtonProps={{ hidden: true }}
+            okButtonProps={{ hidden: true }}
+            centered
+        >
+            {isLoadingAuth ? (
+                <LoadingSpinner />
+            ) : (
+                <UserUpdateComponent onBack={handleClose} language="en" />
+            )}
+        </Modal>
+    );
+}
