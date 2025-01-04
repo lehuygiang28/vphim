82c0402e9a62e07257282693a6f698b77f33a68a
lehuygiang28 <lehuygiang28@gmail.com>
2025-01-05 03:35:00 +0700

    refactor(i18n): :recycle: update user locale import path and remove unused locale service

--- apps/mnt/src/i18n/request.ts
+++ apps/mnt/src/i18n/request.ts
@@ -1,5 +1,5 @@
 import { getRequestConfig } from 'next-intl/server';
-import { getUserLocale } from '../services/locale';
+import { getUserLocale } from '~fe/services/locale';
 
 export default getRequestConfig(async () => {
     const locale = await getUserLocale();
